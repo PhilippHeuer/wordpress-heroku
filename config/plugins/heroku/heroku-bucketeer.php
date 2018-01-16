aec182d428f0996c23cbd717f1d23001fa2f3870
Philipp Heuer <git@philippheuer.me>
2018-01-16 22:20:58 +0100

    fix(bucketeer): removed wrong parameter for the AWS S3 URL

--- config/plugins/heroku/heroku-bucketeer.php
+++ config/plugins/heroku/heroku-bucketeer.php
@@ -5,5 +5,5 @@
  */
 if (!empty(getenv('BUCKETEER_AWS_ACCESS_KEY_ID'))) {
     $region = !empty(getenv('BUCKETEER_REGION')) ? "s3-" + getenv('BUCKETEER_REGION') + ".amazonaws.com" : "s3.amazonaws.com";
-    putenv(sprintf('AWS_S3_URL=s3://%s:%s@%s/%s', $env['user'], getenv("BUCKETEER_AWS_ACCESS_KEY_ID"), getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY"), $region, getenv("BUCKETEER_BUCKET_NAME")));
+    putenv(sprintf('AWS_S3_URL=s3://%s:%s@%s/%s', getenv("BUCKETEER_AWS_ACCESS_KEY_ID"), getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY"), $region, getenv("BUCKETEER_BUCKET_NAME")));
 }
