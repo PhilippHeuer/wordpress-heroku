d46a4b827977146cc7950c82e3e17b75186ea48b
Philipp Heuer <git@philippheuer.me>
2018-01-16 21:57:54 +0100

    fix(bucketeer): add aws region variable

--- config/plugins/heroku/heroku-bucketeer.php
+++ config/plugins/heroku/heroku-bucketeer.php
@@ -4,8 +4,6 @@
  * @url: https://elements.heroku.com/addons/bucketeer
  */
 if (!empty(getenv('BUCKETEER_AWS_ACCESS_KEY_ID'))) {
-    // TODO: Handle custom regions
-    // s3-eu-central-1.amazonaws.com
-    // ...
-    putenv(sprintf('AWS_S3_URL=s3://%s:%s@%s/%s', $env['user'], getenv("BUCKETEER_AWS_ACCESS_KEY_ID"), getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY"), "s3.amazonaws.com", getenv("BUCKETEER_BUCKET_NAME")));
+    $region = !empty(getenv('BUCKETEER_REGION')) ? "s3-" + getenv('BUCKETEER_REGION') + ".amazonaws.com" : "s3.amazonaws.com";
+    putenv(sprintf('AWS_S3_URL=s3://%s:%s@%s/%s', $env['user'], getenv("BUCKETEER_AWS_ACCESS_KEY_ID"), getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY"), $region, getenv("BUCKETEER_BUCKET_NAME")));
 }
