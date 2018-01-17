587d7c9898705ff94581f2ca27a96c2ded54135f
Philipp Heuer <git@philippheuer.me>
2018-01-17 17:27:22 +0100

    fix(bucketeer): extension provides region in var BUCKETEER_AWS_REGION

--- config/plugins/heroku/heroku-bucketeer.php
+++ config/plugins/heroku/heroku-bucketeer.php
@@ -4,6 +4,6 @@
  * @url: https://elements.heroku.com/addons/bucketeer
  */
 if (!empty(getenv('BUCKETEER_AWS_ACCESS_KEY_ID'))) {
-    $region = !empty(getenv('BUCKETEER_REGION')) ? "s3-" + getenv('BUCKETEER_REGION') + ".amazonaws.com" : "s3.amazonaws.com";
+    $region = !empty(getenv('BUCKETEER_AWS_REGION')) ? "s3-" + getenv('BUCKETEER_AWS_REGION') + ".amazonaws.com" : "s3.amazonaws.com";
     putenv(sprintf('AWS_S3_URL=s3://%s:%s@%s/%s', getenv("BUCKETEER_AWS_ACCESS_KEY_ID"), getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY"), $region, getenv("BUCKETEER_BUCKET_NAME")));
 }
